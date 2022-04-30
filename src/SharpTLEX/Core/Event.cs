0d873ecd4ceb891c6203ad81a33224a43d53f719
Codetancy <codetancy@gmail.com>
2022-04-30 15:44:39 -0400

    Created Event and EventClass

---
+++ src/SharpTLEX/Core/Event.cs
@@ -0,0 +1,68 @@
+﻿namespace SharpTLEX.Core;
+
+public class Event : ICloneable, IEquatable<Event?>
+{
+    public Event(int id, EventClass eventClass, string? stem = null)
+    {
+        Id = id;
+        EventClass = eventClass;
+        Stem = stem;
+    }
+
+    public int Id { get; }
+
+    public string StrId => "e" + Id;
+
+    public EventClass EventClass { get; }
+
+    public string? Stem { get; }
+
+    public static bool operator ==(Event? e1, Event? e2)
+    {
+        if (e1 is null)
+            return e2 is null;
+
+        return e1.Equals(e2);
+    }
+
+    public static bool operator !=(Event? e1, Event? e2) => !(e1 == e2);
+
+    public bool Equals(Event? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Id == other.Id && EventClass.Equals(other.EventClass) && Stem == other.Stem;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((Event) obj);
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Id, EventClass.Value, Stem);
+
+    public object Clone() => new Event(Id, EventClass, Stem);
+}
---
+++ src/SharpTLEX/Core/EventClass.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.SmartEnum;
+
+namespace SharpTLEX.Core;
+
+public class EventClass : SmartEnum<EventClass, string>
+{
+    public static readonly EventClass Reporting = new EventClass(nameof(Reporting), "REPORTING");
+    public static readonly EventClass Perception = new EventClass(nameof(Perception), "PERCEPTION");
+    public static readonly EventClass Aspectual = new EventClass(nameof(Aspectual), "ASPECTUAL");
+    public static readonly EventClass IntentionalAction = new EventClass(nameof(IntentionalAction), "I_ACTION");
+    public static readonly EventClass IntentionalState = new EventClass(nameof(IntentionalState), "I_STATE");
+    public static readonly EventClass State = new EventClass(nameof(State), "STATE");
+    public static readonly EventClass Occurrence = new EventClass(nameof(Occurrence), "OCCURRENCE");
+
+    private EventClass(string name, string value) : base(name, value)
+    {
+    }
+}
