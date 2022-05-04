8f9678d807aba8e23503170b822d7cfeae55aa59
Codetancy <codetancy@gmail.com>
2022-05-04 17:11:20 -0400

    Created LinkType union of TLink, SLink, and ALink

---
+++ src/SharpTLEX/Converters/LinkTypeJsonConverter.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using SharpTLEX.Core;
+
+namespace SharpTLEX.Converters;
+
+public class LinkTypeJsonConverter : JsonConverter<LinkType>
+{
+    public override LinkType? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.TokenType == JsonTokenType.Null ? null : GetFromValue(reader.GetString()!);
+    }
+
+    private static LinkType GetFromValue(string value)
+    {
+        if (TLinkType.TryFromValue(value, out TLinkType tLinkType))
+        {
+            return tLinkType;
+        }
+        else if (SLinkType.TryFromValue(value, out SLinkType sLinkType))
+        {
+            return sLinkType;
+        }
+        else if (ALinkType.TryFromValue(value, out ALinkType aLinkType))
+        {
+            return aLinkType;
+        }
+        else
+        {
+            throw new JsonException();
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, LinkType value, JsonSerializerOptions options)
+    {
+        if (value.IsT0)
+        {
+            writer.WriteStringValue(value.AsT0.Value);
+        }
+        else if (value.IsT1)
+        {
+            writer.WriteStringValue(value.AsT1.Value);
+        }
+        else if (value.IsT2)
+        {
+            writer.WriteStringValue(value.AsT2.Value);
+        }
+        else
+        {
+            throw new JsonException();
+        }
+    }
+}
---
+++ src/SharpTLEX/Core/LinkType.cs
@@ -0,0 +1,19 @@
+﻿using OneOf;
+
+namespace SharpTLEX.Core;
+
+public class LinkType : OneOfBase<TLinkType, SLinkType, ALinkType>
+{
+    protected LinkType(OneOf<TLinkType, SLinkType, ALinkType> input) : base(input)
+    {
+    }
+
+    public static implicit operator LinkType(TLinkType tLinkType) => new LinkType(tLinkType);
+    public static explicit operator TLinkType(LinkType linkType) => linkType.AsT0;
+
+    public static implicit operator LinkType(SLinkType sLinkType) => new LinkType(sLinkType);
+    public static explicit operator SLinkType(LinkType linkType) => linkType.AsT1;
+
+    public static implicit operator LinkType(ALinkType aLinkType) => new LinkType(aLinkType);
+    public static explicit operator ALinkType(LinkType linkType) => linkType.AsT2;
+}
